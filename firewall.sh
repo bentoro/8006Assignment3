dc64fb6391218d668ae84be507362b6ab1840434
bentoro <bl_04@hotmail.com>
2018-03-01 09:30:47 -0800

    Added array parsing

--- firewall.sh
+++ firewall.sh
@@ -1,4 +1,5 @@
 IP="/sbin/iptables"
+ssh_array=()
 
 
 #parses the /var/log/secure file for $1 service, and creates an array with the Date, Time, IP address, and port
@@ -8,6 +9,20 @@ parse_logs(){
 
     #find all instances that failed
     cat block_ip | sed 's/ .*//'
+
+    #find all failed attempts within time range
+    start="Feb 28 14:04:31"
+    end="Feb 28 14:04:40"
+
+    cat /var/log/secure | grep $1 | grep 'Failed password for root from' | awk -F: -v start="$start" -v end="$end" '$1<start && $1 <= end' > service_array
+
+    #store the lines into the array
+    while read line
+    do
+      arr=($line)
+      array+=("${arr[0]} ${arr[1]} ${arr[2]} ${arr[10]} ${arr[12]}")
+    done < /var/log/secure
+
 }
 
 #wrapper function to easily analyze date/time format from the /var/log/sercure file
