7f85b452b6515a797c242f8d43c4f2d20068cae9
Aing <aingaran2003@gmail.com>
2018-02-28 17:11:37 -0800

    added template for functions, and /var/log/secure file from lab

--- firewall.sh
+++ firewall.sh
@@ -1,13 +1,34 @@
 IP="/sbin/iptables"
 
-Firewall(){
+
+#parses the /var/log/secure file for $1 service, and creates an array with the Date, Time, IP address, and port
+parse_logs(){
+    #find ip and port of all failed attempts for ssh TODO: add Date and Time to block_ip
+    cat /var/log/secure | grep $1 | grep 'Failed password for root from' | sed 's/^.*from //' > service_array
+
+    #find all instances that failed
+    cat block_ip | sed 's/ .*//'
+}
+
+#wrapper function to easily analyze date/time format from the /var/log/sercure file
+find_date_time() {
+
+}
+
+#wrapper function to easily analyze ip from a line
+find_ip() {
+    #sed
+}
+
+#wrapper function to easily analyze port from a line (extra)
+find_port() {
+
+}
+
+firewall(){
   $IP -A INPUT -s $1 -j DROP
 }
 
-Flush(){
+flush(){
   $IP -D -s $1 -J DROP
-}
-
-Search(){
-  cat /var/log/secure | grep ssh | grep 'Failed password for root from' | sed 's/^.*from //' > block_ip
 }
