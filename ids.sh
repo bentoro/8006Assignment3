68bcea1e313b0290e587ea20a80bdc55e431135f
Aing <aingaran2003@gmail.com>
2018-03-01 16:16:07 -0800

    trying to fix if statement

--- ids.sh
+++ ids.sh
@@ -4,11 +4,6 @@ ssh_array=()
 
 #parses the /var/log/secure file for $1 service, and creates an array with the Date, Time, IP address, and port
 parse_logs(){
-    #find ip and port of all failed attempts for ssh TODO: add Date and Time to block_ip
-    #cat $1 | grep $2 | grep 'Failed password for root from' | sed 's/^.*from //' > block_ip
-
-    #find all instances that failed
-    #cat block_ip | sed 's/ .*//'
 
     #find all failed attempts within time range
     cat $1 | grep $2 | grep 'Failed password for root from' > service_array
@@ -27,11 +22,31 @@ parse_logs(){
       test=$( date -d"$str" +"%s" )
       if [ $test -ge $time1 ] && [ $test -le $time2 ]
       then
-              array+=("${arr[0]} ${arr[1]} ${arr[2]} ${arr[10]} ${arr[12]}")
-              echo $array
+              ssh_array+=("${arr[0]} ${arr[1]} ${arr[2]} ${arr[10]} ${arr[12]}")
       fi
-    done < test.txt
+    done < service_array
+}
 
+block_ip(){
+    #find ip and port of all failed attempts for ssh TODO: add Date and Time to block_ip
+    #cat $1 | grep 'Failed password for root from' | sed 's/^.*from //' > block_ip
+
+    #find all instances that failed
+    #cat block_ip | sed 's/ .*//'
+
+
+
+    cat service_array | grep -Po "[0-9]+\.[0-9]+\.[0-9]+\.[0-9]+" | uniq -c >block_ip
+
+
+    while read line
+    do
+        arr=($line)
+        if [ "${arr[0]}" -gt 3]
+        then
+            echo "blocking ip" ${arr[0]}
+        fi
+    done < block_ip
 }
 
 #wrapper function to easily analyze date/time format from the /var/log/sercure file
@@ -58,3 +73,4 @@ flush(){
 #1 = log file location
 #2 = service
 parse_logs $1 $2
+block_ip service_array $3
