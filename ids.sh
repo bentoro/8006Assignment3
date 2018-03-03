1f29b9df71e3ef2e721f7acfcbbab18fe06507dc
root <bl_04@hotmail.com>
2018-03-02 20:53:39 -0800

    Removed at

--- ids.sh
+++ ids.sh
@@ -1,5 +1,6 @@
 IP="/sbin/iptables"
-
+time1=$(date +%s -d "$3 min ago")
+time2=$(date +%s)
 
 #parses the /var/log/secure file for $1 service, and creates an array with the Date, Time, IP address, and port
 parse_logs(){
@@ -9,18 +10,18 @@ parse_logs(){
 
     #empty the parsed_secure file
     echo > parsed_secure
-    time1=$(date +%s -d "$3 min ago")
-    time2=$(date +%s)
     #parse the failed attempts by date
     while read line
     do
         arr=($line)
         str=("${arr[0]} ${arr[1]} ${arr[2]}")
         test=$( date -d"$str" +"%s" )
+        time=("Mar 2 16:34:17");test=$(date -d"$time 1 minutes"); echo $test
+
         if [ $test -ge $time1 ] && [ $test -le $time2 ]
         then
             echo "found entry at: " $test " against: " $time1
-            echo "${arr[0]} ${arr[1]} ${arr[2]} ${arr[10]} ${arr[12]}" >> parsed_secure
+            echo "${arr[0]} ${arr[1]} ${arr[2]} ${arr[10]}" >> parsed_secure
 
         fi
     done < service_array
@@ -29,10 +30,32 @@ parse_logs(){
 isBlocked(){
   #if the ip exists in the file return true
   if grep -q $1 blocked; then
-    return 0
+    t="$1"
+    #extract the last time the ip was used
+    line=$(tac blocked | grep -m1 $t | sed "s/$t.*//")
+    test=$(date +%s -d"$line $2 minutes")
+    #compare the blocked time + duration with the current time
+    printf "comparing $(date -d"$line") and $(date) \n"
+    if [ $test -ge $time2 ]
+    then
+      #drop the ip because time duration has passed
+        $ip -D INPUT -s $1 -j DROP
+    else
+        return 0
+    fi
   else
+    #false
     return 1
   fi
+}
+unblock(){
+  #check if ips need to be unblocked first
+  while read line
+  do
+    $arr=($line)
+    isBlocked ${arr[3]}
+  fi
+done < blocked
 }
 
 block_ip(){
@@ -44,21 +67,24 @@ block_ip(){
         arr=($line)
         if [ "${arr[0]}" -ge $2 ]
         then
-          #if ip is already blocked dont block it
-          if isBlocked ${arr[1]}; then
+          #1 = ip to be checked if being blocked or not
+          #2 = amount of time
+          if isBlocked ${arr[1]} $1; then
             echo "already blocked"
           else
             #empty the remove.sh file
             echo > remove.sh
             echo "blocking ip" ${arr[1]}
-            #add ip to the blocked list
-            echo ${arr[1]} >> blocked
-            $IP -A INPUT -s ${arr[1]} -j DROP
-            echo "/sbin/iptables -D INPUT -s ${arr[1]} -j DROP" >> remove.sh
-            echo "sed -i "/${arr[1]}/d" blocked" >> remove.sh
-            chmod 755 remove.sh
-            at -f jobs.txt now + $1 minutes
-            echo "at job set to remove block after $1 mins"
+            #add ip to the blocked list and time
+            t="${arr[1]}"
+            line=$(tac parsed_secure | grep -m1 $t | sed "s/$t.*//")
+            echo "$line ${arr[1]}" >> blocked
+            #$IP -A INPUT -s ${arr[1]} -j DROP
+            #echo "/sbin/iptables -D INPUT -s ${arr[1]} -j DROP" >> remove.sh
+            #echo "sed -i "/${arr[1]}/d" blocked" >> remove.sh
+            #chmod 755 remove.sh
+            #at -f jobs.txt now + 1 minutes
+            #echo "at job set to remove block after $1 mins"
           fi
 
         else
@@ -76,6 +102,7 @@ if [ "$#" -ne 4 ]; then
     echo "Usage error: ./ids.sh [log file location] [service to find] [amount of time blocked] [number of failed attempts]"
     exit 1
 fi
-
+#check if ips need to be unblocked
+unblock
 parse_logs $1 $2 $3
 block_ip $3 $4
--- startcron.sh
+++ startcron.sh
@@ -1,4 +1,4 @@
 #echo new cron into cron file
-echo "* * * * * ./ids.sh secure ssh 1 3" >> start
+echo "* * * * * /home/Ben/github/8006Assignment3/test.sh" > start
 #install new cron file
 crontab start
