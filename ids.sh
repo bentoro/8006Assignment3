eb490a425e252547f46ddb235a17074ba7945992
Aing <aingaran2003@gmail.com>
2018-03-04 00:48:03 -0800

    must delete blocked file before using, blocked not being deleted with sed, still need crontab

--- ids.sh
+++ ids.sh
@@ -21,7 +21,8 @@ parse_logs(){
         then
             echo "found entry at: " $test " against: " $time1 >> $log
             echo "${arr[0]} ${arr[1]} ${arr[2]} ${arr[10]} ${arr[12]}" >> parsed_secure
-
+        else
+            echo "found nothing" >> $log
         fi
     done < service_array
 }
@@ -54,9 +55,6 @@ block_ip(){
             $IP -A INPUT -s ${arr[1]} -j DROP
             echo "/sbin/iptables -D INPUT -s ${arr[1]} -j DROP" >> remove.sh
             echo "sed -i "/${arr[1]}/d" blocked" >> remove.sh
-            #chmod 755 remove.sh
-            #at -f jobs.txt now + $1 minutes
-            #$echo "at job set to remove block after $1 mins" >> $log
           fi
 
         else
@@ -66,23 +64,42 @@ block_ip(){
     done < block_ip
 }
 
+unblock(){
+    while read line
+    do
+        attempted=$( grep -c $line parsed_secure)
+        echo $line attempted: $attempted/$1 times >>$log
+        if [ $attempted -lt $1 ]
+        then
+            echo clearing: $line >> $log
+            $IP -D INPUT -s $line -j DROP
+            sed -i.bak '/$line/d' blocked
+        fi
+    done < blocked
+}
+
 #1 = log file location
 #2 = service
 #3 = time to check
 #4 = number of failed attempts before blocking ip
 if [ "$#" -ne 4 ]; then
-    echo "Usage error: ./ids.sh [log file location] [service to find] [amount of time blocked] [number of failed attempts]" >> log
+    echo "Usage error: ./ids.sh [log file location] [service to find] [amount of time blocked] [number of failed attempts]" >> $log
     exit 1
 fi
 
-rm -f blocked
+#rm -f blocked
 rm -f block_ip
 rm -f parsed_secure
 rm -f service_array
 rm -f log.txt
 echo > log.txt
-echo > blocked
+#echo > blocked
 
-
-parse_logs $1 $2 $3
-block_ip $3 $4
+while true
+do
+    parse_logs $1 $2 $3
+    block_ip $3 $4
+    #after blocked and block_ip are updated
+    unblock $4
+    sleep 5
+done
--- startcron.sh
+++ startcron.sh
@@ -1,4 +1,31 @@
-/home/aing/git/8006/8006Assignment3/ids.sh /home/aing/git/8006/8006Assignment3/secure ssh 2 3
-echo "* * * * * /home/aing/git/8006/8006Assignment3/ids.sh /home/aing/git/8006/8006Assignment3/secure ssh 2 3" > start
+#User configuration---------------------------------
+
+#IDS script location:
+script=/home/aing/git/8006/8006Assignment3/ids.sh
+
+#Security log location:
+log=/home/aing/git/8006/8006Assignment3/secure
+
+#Service to monitor
+service=ssh
+
+#Duration to block
+duration=2
+
+#Failure tolerance
+tolerance=3
+
+#---------------------------------------------------
+
+#/home/aing/git/8006/8006Assignment3/ids.sh /home/aing/git/8006/8006Assignment3/secure ssh 2 3
+
+#run IDS immediately after starting cron job
+echo "* * * * * $script $log $service $duration $tolerance" > start
+
+#echo "* * * * * /home/aing/git/8006/8006Assignment3/ids.sh /home/aing/git/8006/8006Assignment3/secure ssh 2 3" > start
+
 #install new cron file
 crontab start
+
+
+#$script $log $service $duration $tolerance
